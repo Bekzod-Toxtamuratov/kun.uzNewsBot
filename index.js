4fc6d18184920dadb3deca80a52cd17292ef12c4
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-08-05 10:09:23 +0300

    ok

--- index.js
+++ index.js
@@ -2,22 +2,10 @@
 import fetch from 'node-fetch'
 import TelegramBot from 'node-telegram-bot-api'
 
-const path = require('path')
-
-module.exports = {
-	// Other configurations...
-	resolve: {
-		alias: {
-			punycode: path.resolve(__dirname, 'node_modules/punycode'),
-		},
-	},
-}
-
 const TOKEN = '7028225478:AAGJCVgW6EkIuogTDy5Y-nr7oyZmbgF4loU'
 const CHAT_ID = '5577209891' // Your Telegram chat ID
 // Create a bot instance
 const bot = new TelegramBot(TOKEN)
-
 async function fetchAndSendNews() {
 	try {
 		const response = await fetch('https://kun.uz/news/list?f=latest&l=5', {
@@ -26,7 +14,6 @@ async function fetchAndSendNews() {
 				'x-requested-with': 'XMLHttpRequest',
 			},
 		})
-
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`)
 		}
